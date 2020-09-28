8f1ee65e977d6676e3a3d472fa56a94cec76b08e
Yan <41467560+yanissohappy@users.noreply.github.com>
2020-09-29 00:59:04 +0800

    Prototype but still not correct answer

--- hw2/hw2.py
+++ hw2/hw2.py
@@ -1,6 +1,7 @@
 import cv2
 import matplotlib.pyplot as plt
 import copy
+import numpy as np
 
 img = cv2.imread('lena.bmp', cv2.IMREAD_GRAYSCALE)
 length = len(img) # row
@@ -29,64 +30,8 @@ def newLabel():
 	label += 1
 	return label
 	
-def setValue(image, row, col):
-	if row == 0 and col == 0: # top 1st index
-		return newLabel()
-	if row == 0: # top beside 1st index
-		_min = image[row][col - 1]
-		if _min != 0:
-			return _min
-		else:
-			return newLabel()
-	if row != 0 and col == 0: # left beside 1st index
-		temp = [image[row - 1][col], image[row - 1][col + 1]]
-		if temp.count(0) == 2:
-			return newLabel()
-		else: # At least one of them is not 0
-			if temp.count(0) == 1:
-				temp.remove(0)
-				return min(temp)
-			elif temp.count(0) == 0:
-				return min(temp)
-			
-	if row != 0 and col == width - 1: # right not first row
-		temp = [image[row - 1][col - 1], image[row - 1][col], image[row][col - 1]]
-		if temp.count(0) == 3:
-			return newLabel()
-		else: # At least one of them is not 0
-			if temp.count(0) == 2:
-				temp.remove(0)
-				temp.remove(0)
-				return min(temp)
-			elif temp.count(0) == 1:
-				temp.remove(0)
-				return min(temp)
-			elif temp.count(0) == 0:
-				return min(temp)
-				
-	# other cases
-	temp = [image[row - 1][col - 1], image[row - 1][col], image[row - 1][col + 1], image[row][col - 1]]
-	if temp.count(0) == 4:
-		return newLabel()
-	else: # At least one of them is not 0
-		if temp.count(0) == 3:
-			temp.remove(0)
-			temp.remove(0)
-			temp.remove(0)
-			return temp[0]
-		elif temp.count(0) == 2:
-			temp.remove(0)
-			temp.remove(0)
-			return min(temp)
-		elif temp.count(0) == 1:
-			temp.remove(0)
-			return min(temp)
-		elif temp.count(0) == 0:
-			return min(temp)
-	
 def makeLabel(image):
 	ret = image.copy()
-	# ret = [[0 for _ in range(int(width))] for _ in range(int(length))]
 	# 1st pass, scanning and make new label
 	for i in range(int(length)):
 		for j in range(int(width)):
@@ -96,8 +41,7 @@ def makeLabel(image):
 				ret[i][j] = newLabel()
 	return ret
 
-def firstPass(image, equivalence_list):
-	is_revised = 0
+def firstPass(image):
 	for i in range(int(length)):
 		for j in range(int(width)):
 			if image[i][j] != 0: # 255
@@ -119,39 +63,42 @@ def firstPass(image, equivalence_list):
 						_min = image[i][j - 1]
 						is_checked = 1
 				if _min != image[i][j]:
-					equivalence_list[image[i][j]] = _min # change equivalence table
 					image[i][j] = _min	
-					is_revised = 1
-	if is_revised == 1:
-		return True
-	else:
-		return False
 
-def secondPass(image, equivalence_list):	# adjust all equivalence_list
+def secondPass(image):	# adjust all equivalence_list
+	for i in range(int(length) - 1, -1, -1):
+		for j in range(int(width) - 1, -1, -1):
+			if image[i][j] != 0: # 255
+				_min, is_checked = image[i][j], 0
+				if i != 0 and j != 0: # left upper
+					if image[i - 1][j - 1] != 0 and image[i - 1][j - 1] < _min and is_checked != 1:
+						_min = image[i - 1][j - 1]
+						is_checked = 1
+				if i != 0: # upper
+					if image[i - 1][j] != 0 and image[i - 1][j] < _min and is_checked != 1:
+						_min = image[i - 1][j]
+						is_checked = 1
+				if i != 0 and j != int(length) - 1: # right upper
+					if image[i - 1][j + 1] != 0 and image[i - 1][j + 1] < _min and is_checked != 1:
+						_min = image[i - 1][j + 1]
+						is_checked = 1
+				if j != 0: # just left
+					if image[i][j - 1] != 0 and image[i][j - 1] < _min and is_checked != 1:
+						_min = image[i][j - 1]
+						is_checked = 1
+				if _min != image[i][j]:
+					image[i][j] = _min	
+
+def countPixelandSetThreshold(image, pixel_count):
 	for i in range(int(length)):
 		for j in range(int(width)):
-			if image[i][j] != 0: # 255
-				temp = equivalence_list[image[i][j]]
-				if i != 0 and j != 0: # left upper
-					if equivalence_list[image[i - 1][j - 1]] != temp:
-						equivalence_list[image[i - 1][j - 1]] = temp
-				if i != 0: # upper
-					if equivalence_list[image[i - 1][j]] != temp:
-						equivalence_list[image[i - 1][j]] = temp
-				if i != 0 and j != int(length) - 1: # right upper
-					if equivalence_list[image[i - 1][j + 1]] != temp:
-						equivalence_list[image[i - 1][j + 1]] = temp
-				if j != 0: # just left
-					if equivalence_list[image[i][j - 1]] != temp:
-						equivalence_list[image[i][j - 1]] = temp
-
-def makeConnectedComponent(image, equivalence_list, pixel_count_for_a_connected):
+			if image[i][j] != 0: # 255	
+				pixel_count[image[i][j]] += 1
 	for i in range(int(length)):
-		for j in range(int(width)):	
-			if image[i][j] != 0: # 255
-				image[i][j] = equivalence_list[image[i][j]]
-				pixel_count_for_a_connected[image[i][j]] += 1
-						
+		for j in range(int(width)):
+			if pixel_count[image[i][j]] < 500: # threshold
+				image[i][j] = 0
+				
 # binary picture output
 binarized_list = binarize(img)
 cv2.imwrite('binary.jpg', binarized_list)
@@ -163,15 +110,38 @@ plt.savefig("histogram.png")
 
 # Connected Component
 ConnectedComponent = makeLabel(binarized_list)
-print(ConnectedComponent)
-equivalence_list = [i for i in range(label + 100)] # used for find the same min value in a pass
+back_up = copy.deepcopy(ConnectedComponent)
+
+# reorganize ConnectedComponent
 while True:
-	#print("X")
-	is_loop_again = firstPass(ConnectedComponent, equivalence_list)
-	secondPass(ConnectedComponent, equivalence_list)
-	if is_loop_again == True:
-		continue
-	else:
-		break
-pixel_count_for_a_connected = [0 for _ in range(label + 100)] # used for counting number of a connected component
-makeConnectedComponent(ConnectedComponent, equivalence_list, pixel_count_for_a_connected)
+	is_loop_again = firstPass(ConnectedComponent)
+	secondPass(ConnectedComponent)
+	if False not in (ConnectedComponent == back_up):
+		break	
+	back_up = copy.deepcopy(ConnectedComponent)
+	
+# threshold == 500
+pixel_count = [0 for _ in range(np.max(ConnectedComponent) + 1)]
+countPixelandSetThreshold(ConnectedComponent, pixel_count)
+for i in range(len(pixel_count)):
+	if pixel_count[i] >= 500:
+		sum_j, sum_k = 0, 0 # for centroid use
+		top, bottom, left, right = length, -1, width, -1 # initial value
+		for j in range(int(length)):
+			for k in range(int(width)):
+				if (ConnectedComponent[j][k] == i):
+					sum_j += j
+					sum_k += k
+					if j < top:
+						top = j
+					if j > bottom:
+						bottom = j
+					if k < left:
+						left = k
+					if k > right:
+						right = k	
+		row_centroid = round(sum_j / pixel_count[i])
+		col_centroid = round(sum_k / pixel_count[i])
+		cv2.rectangle(binarized_list, (left, top), (right, bottom), (127, 0, 0), 3)
+		cv2.circle(binarized_list, (col_centroid, row_centroid), 4, (127, 0, 0), -1)
+cv2.imwrite('boundingBox.jpeg', binarized_list)
