a82b1c117a9619cbc65b60dd7f9343d0e47d3602
Yan <41467560+yanissohappy@users.noreply.github.com>
2020-09-30 02:45:42 +0800

    Revise hw2

--- hw2/hw2.py
+++ hw2/hw2.py
@@ -9,7 +9,7 @@ width = len(img[0]) # col, it's actually a square though
 label = 0
 
 def binarize(image):
-	# ret = image.copy() # Here is really important!!!!!!
+	# ret = image.copy() # can't use .copy or bounding box will be wrong
 	ret = np.zeros(image.shape, np.int)
 	for i in range(int(width)):
 		for j in range(int(length)):
@@ -30,17 +30,70 @@ def newLabel():
 	global label
 	label += 1
 	return label
+
+def setValue(image, row, col):
+	if row == 0 and col == 0: # top
+		return newLabel()
+	if row == 0: # top beside 1st index
+		_min = image[row][col - 1]
+		if _min != 0:
+			return _min
+		else:
+			return newLabel()
+	if row != 0 and col == 0: # left beside 1st index
+		temp = [image[row - 1][col], image[row - 1][col + 1]]
+		if temp.count(0) == 2:
+			return newLabel()
+		else: # At least one of them is not 0
+			if temp.count(0) == 1:
+				temp.remove(0)
+				return min(temp)
+			elif temp.count(0) == 0:
+				return min(temp)
+			
+	if row != 0 and col == width - 1: # right not first row
+		temp = [image[row - 1][col - 1], image[row - 1][col], image[row][col - 1]]
+		if temp.count(0) == 3:
+			return newLabel()
+		else: # At least one of them is not 0
+			if temp.count(0) == 2:
+				temp.remove(0)
+				temp.remove(0)
+				return min(temp)
+			elif temp.count(0) == 1:
+				temp.remove(0)
+				return min(temp)
+			elif temp.count(0) == 0:
+				return min(temp)
+				
+	# other cases
+	temp = [image[row - 1][col - 1], image[row - 1][col], image[row - 1][col + 1], image[row][col - 1]]
+	if temp.count(0) == 4:
+		return newLabel()
+	else: # At least one of them is not 0
+		if temp.count(0) == 3:
+			temp.remove(0)
+			temp.remove(0)
+			temp.remove(0)
+			return temp[0]
+		elif temp.count(0) == 2:
+			temp.remove(0)
+			temp.remove(0)
+			return min(temp)
+		elif temp.count(0) == 1:
+			temp.remove(0)
+			return min(temp)
+		elif temp.count(0) == 0:
+			return min(temp)
 	
 def makeLabel(image):
-	ret = image.copy()
-	label = 1
+	# ret = image.copy()
+	ret = np.zeros(image.shape, np.int)
 	# 1st pass, scanning and make new label
 	for i in range(int(length)):
 		for j in range(int(width)):
 			if image[i][j] != 0: # 255
-				# ret[i][j] = newLabel()
-				ret[i][j] = label
-				label += 1
+				ret[i][j] = setValue(ret, i, j)
 	return ret
 
 def firstPass(image):
@@ -115,14 +168,21 @@ def countPixelandSetThreshold(image, pixel_in_a_class):
 		for j in range(int(width)):
 			if image[i][j] != 0: 
 				pixel_in_a_class[image[i][j]] += 1
-	for i in range(int(length)):
-		for j in range(int(width)):
-			if pixel_in_a_class[image[i][j]] < 500: # threshold
-				image[i][j] = 0
+
+def updateBoxBorder(top, bottom, left, right, i, j):
+	if i < top:
+		top = i
+	if i > bottom:
+		bottom = i
+	if j < left:
+		left = j
+	if j > right:
+		right = j		
+	return top, bottom, left, right
 				
 # binary picture output
 binarized_photo = binarize(img)
-cv2.imwrite('binary.jpg', binarized_photo)
+cv2.imwrite('binary.png', binarized_photo)
 
 # histogram picture output
 pixel_count = histogramPixelCount(img)
@@ -131,12 +191,11 @@ plt.savefig("histogram.png")
 
 # Connected Component
 ConnectedComponent = makeLabel(binarized_photo)
-print(ConnectedComponent)
 back_up = copy.deepcopy(ConnectedComponent)
 
 # reorganize ConnectedComponent
 while True:
-	print("Converging...")
+	print("Connected Component Label Propagating and Converging...")
 	firstPass(ConnectedComponent)
 	secondPass(ConnectedComponent)
 	if False not in (ConnectedComponent == back_up):
@@ -146,32 +205,19 @@ while True:
 # threshold == 500
 pixel_in_a_class = [0 for _ in range(np.max(ConnectedComponent) + 1)]
 countPixelandSetThreshold(ConnectedComponent, pixel_in_a_class)
-print(pixel_in_a_class)
+
 for i in range(len(pixel_in_a_class)):
 	if pixel_in_a_class[i] >= 500:
 		sum_j, sum_k = 0, 0 # for centroid use
 		top, bottom, left, right = length, -1, width, -1 # initial value
 		for j in range(int(length)):
 			for k in range(int(width)):
-				if (ConnectedComponent[j][k] == i):
+				if (ConnectedComponent[j][k] == i): # if belongs to that label
 					sum_j += j
 					sum_k += k
-					if j < top:
-						top = j
-					if j > bottom:
-						bottom = j
-					if k < left:
-						left = k
-					if k > right:
-						right = k	
+					top, bottom, left, right = updateBoxBorder(top, bottom, left, right, j, k)
 		row_centroid = round(sum_j / pixel_in_a_class[i])
 		col_centroid = round(sum_k / pixel_in_a_class[i])
-		cv2.rectangle(binarized_photo, (left, top), (right, bottom), (127, 0, 0), 3)
-		cv2.circle(binarized_photo, (col_centroid, row_centroid), 4, (127, 0, 0), -1)
-cv2.imwrite('boundingBox.jpeg', binarized_photo)
-
-"""
-cv2.imshow('My Image', binarized_photo)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-"""
+		cv2.rectangle(binarized_photo, (left, top), (right, bottom), (127, 0, 0), 2)
+		cv2.circle(binarized_photo, (col_centroid, row_centroid), 5, (127, 0, 0), -1) # centroid = x, y = (col, row)
+cv2.imwrite('boundingBox.png', binarized_photo)
