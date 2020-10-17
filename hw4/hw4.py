67999677a9919bd5576ad9a1df49eade4a8c4779
Yan <41467560+yanissohappy@users.noreply.github.com>
2020-10-17 09:45:40 +0800

    Add erosion but not sure

--- hw4/hw4.py
+++ hw4/hw4.py
@@ -38,11 +38,29 @@ def dilation(image, kernel):
 					scope_j = j + kernel_j - 2
 					if kernel[kernel_i, kernel_j] == 1: # if some position is 1 in structure component, then do the following
 						if (0 <= scope_i and scope_i < length) and (0 <= scope_j and scope_j < width):
-							if image[scope_i][scope_j] == 255: # if the position kernel == 1 and one of the 
+							if image[scope_i][scope_j] == 255:
 								value = 255 # make it white
 			ret[i, j] = value
 	return ret
-					
+
+def erosion(image, kernel):
+	ret = np.zeros(image.shape, np.int)
+	# center of kernel will be put from [[2, width - 1 - 2], [2, length - 1 - 2]] becuz kernel center is in (2, 2)
+	# hence, I can imagine kernel (0, 0) will be put in the (-2, -2) of the original photo (Lena) 
+	for i in range(int(width)):
+		for j in range(int(length)):
+			value = 255
+			for kernel_i in range(5):
+				for kernel_j in range(5):
+					scope_i = i + kernel_i - 2
+					scope_j = j + kernel_j - 2
+					if kernel[kernel_i, kernel_j] == 1: # if some position is 1 in structure component, then do the following
+						if (0 <= scope_i and scope_i < length) and (0 <= scope_j and scope_j < width):
+							if image[scope_i][scope_j] == 0: # if one of kernel touches black part
+								value = 0 # make it black
+			ret[i, j] = value
+	return ret
+	
 ''' test another way for dilation
 def try_d(image, kernel):
 	ret = np.zeros(image.shape, np.int)
@@ -65,3 +83,6 @@ binarized_photo = binarize(img)
 # dilation
 dilated_photo = dilation(binarized_photo, kernel)
 cv2.imwrite('dilation.png', dilated_photo)
+# erosion
+eroted_photo = erosion(binarized_photo, kernel)
+cv2.imwrite('erosion.png', eroted_photo)
